63ce9e812b60245e5eb17e45c4ce49f8de52b930
George Wu <stupidfive@gmail.com>
2015-09-18 20:44:31 +0800

    Empty project commit.

---
+++ SwiftDataStructuresAndAlgorithms/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SwiftDataStructuresAndAlgorithms
+//
+//  Created by George Wu on 9/18/15.
+//  Copyright © 2015 George Wu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
